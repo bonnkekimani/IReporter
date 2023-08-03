7008b6f698f51ead19db214d1098f01cf4cf9ce9
MORGANJASON <420morganjason@gmail.com>
2023-08-03 22:36:03 +0300

    Admin SIdebar

--- FRONT-END/my-app/src/Sidebar.js
+++ FRONT-END/my-app/src/Sidebar.js
@@ -1,33 +1,40 @@
-import React, { useState } from 'react';
-// import './Sidebar.css'; // Create a CSS file for sidebar styling
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+// import "./Sidebar.css"; // Import the CSS file for sidebar styles
 
 const Sidebar = () => {
-    const [status, setStatus] = useState('Under Investigation');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const handleStatusChange = (newStatus) => {
-        setStatus(newStatus);
+    const handleLogout = () => {
+        // Perform any logout actions here, e.g., clearing user data from localStorage
+        setIsLoggedIn(false);
+        alert("You have logged out.");
     };
 
     return (
         <div className="sidebar">
-        <div className="logo">I-Reporter</div>
+        <div className="logo">I REPORTER</div>
         <ul className="menu">
-            <li className="menu-item">View Reports</li>
-            <li className="menu-item">Logout</li>
-            <li
-            className="menu-item"
-            style={{
-                color:
-                status === 'Under Investigation'
-                    ? 'black'
-                    : status === 'Rejected'
-                    ? 'red'
-                    : 'green',
-            }}
-            onClick={() => handleStatusChange('Under Investigation')}
-            >
-            Change Status
+            <li className="menu-item">
+            <NavLink exact to="/" activeClassName="active">
+                Admin Login
+            </NavLink>
             </li>
+            <li className="menu-item">
+            <NavLink to="/Admin" activeClassName="active">
+                View Reports
+            </NavLink>
+            </li>
+            <li className="menu-item">
+            <NavLink to="/CardsContainer" activeClassName="active">
+                Cards Container
+            </NavLink>
+            </li>
+            {isLoggedIn ? (
+            <li className="menu-item">
+                <button onClick={handleLogout}>Logout</button>
+            </li>
+            ) : null}
         </ul>
         </div>
     );
