240d2e30ec18ba927d4629107dd24733d78e643f
+ <+>
2023-08-30 04:53:02 +0300

    front-end

--- BACK-END/config.py
+++ BACK-END/config.py
@@ -8,6 +8,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONs=config('SQLALCHEMY_TRACK_MODIFICATIONS',cast=bool)
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI ="sqlite:///"+os.path.join(BASE_DIR, 'report.db')
+    SQLALCHEMY_DATABASE_URI ="postgresql://ireporter_db_user:feX1agtrdJN9Wtjaa6CRCSQcXCxpZsZT@dpg-cjms21fjbvhs73ecjqq0-a.oregon-postgres.render.com/ireporter_db"
+    # +os.path.join(BASE_DIR, 'report.db')
     DEBUG=True
     SQLALCHEMY_ECHO = True
